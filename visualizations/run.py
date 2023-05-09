85d328d121b25687310d0b6b5865596e691b5044
apkomarla <info@covidincustody.org>
2023-05-09 16:14:05 -0700

    Adding bar graphs

--- visualizations/run.py
+++ visualizations/run.py
@@ -45,7 +45,9 @@ x = df_filter['Transparency Scores']
 sns.barplot(x=x, y=y, palette=gen_palette(x_val = x))  
 
 
-# Cases plot
+
+
+# Point in time cases plot
 
 # Transparency scores per county    
 f, ax = plt.subplots(figsize=(7, 20))  
@@ -72,6 +74,36 @@ df_filter.sort_values(by = 'Transparency Scores', ascending = False, inplace = T
 # Plot the data transparency score (%) per county
 y = df_filter['County'] 
 x = df_filter['Transparency Scores']
-sns.barplot(x=x, y=y, palette=gen_palette(x_val = x))
+sns.barplot(x=x, y=y, palette=gen_palette(x_val = x)).set_title('Positive Cases - Point in time (>1 data point)')
 
 
+
+
+# Historical cases plot
+
+# Transparency scores per county    
+f, ax = plt.subplots(figsize=(7, 20))  
+
+# Generate data for plot 
+selected_vars = select_vars(vars_list = select_numerical_vars(vars_list = df_scores.columns), 
+                            keywords = ['Cases'], 
+                            point_in_time = False, 
+                            historical = True, 
+                            staff = True, 
+                            population = True)
+print('Variables included in the plot are:', selected_vars)
+
+# Store filtered data
+selected_vars.append('County')
+df_filter = pd.DataFrame()
+df_filter = df_scores[selected_vars]
+
+# Calculate percentage of data variables reported by a county in the category
+transparency_scores = list((df_filter.sum(axis = 1, numeric_only = True)/(len(selected_vars)-1))*100)
+df_filter['Transparency Scores']  = transparency_scores
+df_filter.sort_values(by = 'Transparency Scores', ascending = False, inplace = True)
+
+# Plot the data transparency score (%) per county
+y = df_filter['County'] 
+x = df_filter['Transparency Scores']
+sns.barplot(x=x, y=y, palette=gen_palette(x_val = x)).set_title('Positive Cases - Historical (>50 data points)')
